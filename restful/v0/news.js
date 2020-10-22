2de2c7e9c1c8e0015c4ec934d4e4505cce49c227
rackyz <rackyz@qq.com>
2020-10-23 07:18:34 +0800

    news

---
+++ restful/v0/news.js
@@ -0,0 +1,30 @@
+let out = {}
+
+
+out.List = ctx=>{
+  return [{
+    title: '2020年度余姚市流动人员 公共租赁住房租赁补贴申请开始',
+    date: '10-21',
+    url: 'http://zjw.ningbo.gov.cn/art/2020/10/21/art_1229126187_58917366.html'
+
+  },{
+    title: '拥抱“新基建”，更需“新安全”——市城建发展研究中心组织参观“新窗口•新基建•新安全”',
+    date:'10-21',
+    url: 'http://zjw.ningbo.gov.cn/art/2020/10/21/art_1229126187_58917364.html'
+  }, {
+    title: '拥抱“新基建”，更需“新安全”——市城建发展研究中心组织参观“新窗口•新基建•新安全”',
+    date: '10-21',
+    url: 'http://zjw.ningbo.gov.cn/art/2020/10/21/art_1229126187_58917364.html'
+  }, {
+    title: '拥抱“新基建”，更需“新安全”——市城建发展研究中心组织参观“新窗口•新基建•新安全”',
+    date: '10-21',
+    url: 'http://zjw.ningbo.gov.cn/art/2020/10/21/art_1229126187_58917364.html'
+  }, {
+    title: '拥抱“新基建”，更需“新安全”——市城建发展研究中心组织参观“新窗口•新基建•新安全”',
+    date: '10-21',
+    url: 'http://zjw.ningbo.gov.cn/art/2020/10/21/art_1229126187_58917364.html'
+  }]
+
+}
+
+module.exports = out
---
+++ restful/v0/notices.js
@@ -0,0 +1,20 @@
+let out = {}
+
+
+out.List = ctx => {
+  return [{
+    id:1,
+    title: '2021年元旦放假的通知',
+    date: '12-31',
+    url: 'http://zjw.ningbo.gov.cn/art/2020/10/21/art_1229126187_58917366.html'
+
+  }, {
+    id:2,
+    title: '关于企业信息平台上线试运行的通知',
+    date: '11-11',
+    url: 'http://zjw.ningbo.gov.cn/art/2020/10/21/art_1229126187_58917364.html'
+  }]
+
+}
+
+module.exports = out
