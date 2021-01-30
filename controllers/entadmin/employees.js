bc25892fe1f236e5b1a10432bc49d58c63ef9da9
rackyz <rackyz@qq.com>
2021-01-31 07:02:12 +0800

    4123

--- controllers/entadmin/employees.js
+++ controllers/entadmin/employees.js
@@ -1,4 +1,4 @@
-let {Account,Enterprise,Employee} = require('../../models')
+let {Account,Enterprise} = require('../../models')
 let out = {}
 
 
@@ -7,22 +7,38 @@ out.List = async ctx=>{
   return await Account.ListUsersByEnterprise(ent_id)
 }
 
+
 out.Post = async ctx=>{
-  let state = ctx.state
+  let ent_id =  ctx.state.enterprise_id
   let data = ctx.request.body
-  Employee.Create(state,data)
+  let updateInfo = await Account.create(data)
+  await Enterprise.addEnterprise(updateInfo.id,ent_id)
+  return updateInfo
 }
 
 out.Patch = async ctx=>{
-  let state = ctx.state
-  let id =  ctx.params.id
+  let id = ctx.params.id
+  let ent_id =  ctx.state.enterprise_id
   let data = ctx.request.body
-  Employee.Update(state,id,data)
+  let updateInfo = await Account.update(id,data,ctx.state.id)
+  return updateInfo
 }
 
 out.Delete = async ctx=>{
   let id = ctx.params.id
-  Employee.Delete(state,id)
+  let ent_id =  ctx.state.enterprise_id
+  await Enterprise.removeEnterprise(id,ent_id)
 }
 
+
+out.PostAction = async ctx=>{
+  let action = ctx.params.action
+  let ent_id = ctx.state.enterprise_id
+  let data = ctx.request.body
+  if(action == 'delete'){
+    await Enterprise.removeEnterprises(data,ent_id)
+  }else if(action == 'post'){
+    await Account.createAccounts(data,ctx.state.id)
+  }
+}
 module.exports = out
